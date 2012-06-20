eeeb687d824b77d3f7705a7c8c93145ee9b0aec9
Roman <roman@burningswell.com>
2012-06-20 20:07:50 +0200

    Added util ns.

---
+++ src/clj/routes/util.clj
@@ -0,0 +1,51 @@
+(ns routes.util
+  (:refer-clojure :exclude (replace))
+  (:use [clojure.string :only (blank? join lower-case split replace replace-first)]
+        [inflections.core :only (parameterize)]))
+
+(def ^:dynamic *server*
+  {:scheme :https :server-name "example.com"})
+
+(defn identifier [resource keyseq]
+  (->> (map resource keyseq)
+       (map str)
+       (remove blank?)
+       (map lower-case)
+       (map parameterize)
+       (join "-")))
+
+(defn path
+  "Make a path by joining `segments` with a slash."
+  [& segments]
+  (->> (map str segments)
+       (remove blank?)
+       (map #(replace %1 #"^/+" ""))
+       (join "/")
+       (str "/")))
+
+(defn parse-keys [pattern]
+  (->> (split pattern #"/")
+       (map (fn [segment]
+              (->> (re-seq #"\:[^:]+" segment)
+                   (map #(replace %1 ":" ""))
+                   (map #(replace %1 #"-$" ""))
+                   (map keyword)
+                   (apply vector))))
+       (remove empty?)
+       (apply vector)))
+
+(defn format-pattern [pattern & args]
+  (reduce
+   (fn [pattern [keyseq arg]]
+     (reduce #(replace-first
+               %1 (str %2)
+               (parameterize (lower-case (str (get arg %2)))))
+             pattern keyseq))
+   pattern (map vector (parse-keys pattern) args)))
+
+(defn server-url []
+  (let [{:keys [scheme server-name server-port]} *server*]
+    (assert scheme)
+    (assert server-name)
+    (str (name scheme) "://" server-name
+         (if server-port (str ":" server-port)))))
---
+++ test/clj/routes/test/util.clj
@@ -0,0 +1,62 @@
+(ns routes.test.util
+  (:use clojure.test
+        routes.util))
+
+(deftest test-format-pattern
+  (are [pattern args expected]
+    (is (= expected (apply format-pattern pattern args)))
+    "/continents/:iso-3166-1-alpha-2-:name"
+    [{:iso-3166-1-alpha-2 "eu" :name "Europe"}]
+    "/continents/eu-europe"
+    "/:a/:b-:c/d/:e-f-g"
+    [{:a 1} {:b 2 :c 3} {:e-f-g 4}]
+    "/1/2-3/d/4"))
+
+(deftest test-identifier
+  (are [test expected]
+    (is (= expected test))
+    (identifier
+     {:iso-3166-1-alpha-2 "eu"}
+     [:iso-3166-1-alpha-2 :name])
+    "eu"
+    (identifier
+     {:iso-3166-1-alpha-2 "eu"
+      :name "Europe"}
+     [:iso-3166-1-alpha-2 :name])
+    "eu-europe"
+    (identifier
+     {:iso-3166-1-alpha-2 "na"
+      :name "North America"}
+     [:iso-3166-1-alpha-2 :name])
+    "na-north-america"
+    (identifier
+     {:id 1 :name "Northern California"}
+     [:id :name])
+    "1-northern-california"))
+
+(deftest test-path
+  (are [segments expected]
+    (is (= expected (apply path segments)))
+    ["continents"]
+    "/continents"
+    ["/continents"]
+    "/continents"
+    ["//continents"]
+    "/continents"
+    ["continents" "eu-europe"]
+    "/continents/eu-europe"
+    ["/continents" "/eu-europe"]
+    "/continents/eu-europe"))
+
+(deftest test-parse-keys
+  (are [pattern expected]
+    (is (= expected (parse-keys pattern)))
+    "/continents/:iso-3166-1-alpha-2-:name"
+    [[:iso-3166-1-alpha-2 :name]]
+    "/continents/:iso-3166-1-alpha-2-:name"
+    [[:iso-3166-1-alpha-2 :name]]
+    "/:a/:b-:c/d/:e-f-g"
+    [[:a] [:b :c] [:e-f-g]]))
+
+(deftest test-server-url
+  (is (= "https://example.com" (server-url))))
