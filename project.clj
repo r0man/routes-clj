2f0599d6c9bfaea834b68a41b3294f338599cc6b
Roman <roman@burningswell.com>
2012-06-20 20:06:02 +0200

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,33 @@
+(defproject routes-clj "0.0.1-SNAPSHOT"
+  :description "A HTTP route library for Clojure & ClojureScript."
+  :url "http://github.com/r0man/routes-clj"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[inflections "0.7.0-SNAPSHOT"]
+                 [org.clojure/clojure "1.4.0"]]
+  :plugins [[lein-cljsbuild "0.2.1"]]
+  :hooks [leiningen.cljsbuild]
+  :cljsbuild {:builds [{:source-path "src/cljs"
+                        :compiler {:output-to "target/routes-debug.js"}}
+                       {:compiler {:output-to "target/routes.js"
+                                   :optimizations :advanced
+                                   :pretty-print false}
+                        :source-path "src/cljs"}
+                       {:compiler {:output-to "target/routes-test.js"
+                                   ;; :optimizations :whitespace
+                                   :optimizations :advanced
+                                   :pretty-print true}
+                        :jar true
+                        :source-path "test/cljs"}]
+              :crossover-jar true
+              :crossover-path ".crossover-cljs"
+              :crossovers [routes.core
+                           routes.util
+                           routes.routes]
+              :repl-listen-port 9000
+              :repl-launch-commands
+              {"chromium" ["chromium" "http://localhost:9000/"]
+               "firefox" ["firefox" "http://http://localhost:9000/"]}
+              :test-commands {"unit" ["./test-cljs.sh"]}}
+  :source-paths ["src/clj"]
+  :test-paths ["test/clj"])
