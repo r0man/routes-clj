04fdc761558dc0416bd50eaf36c445f8d3c451f0
Roman <roman@burningswell.com>
2012-06-26 22:42:26 +0200

    Assert server-name in server-url fn.

--- src/clj/routes/server.clj
+++ src/clj/routes/server.clj
@@ -8,5 +8,6 @@
 (defn server-url
   "Returns the url of `server`."
   [{:keys [scheme server-name server-port] :as server}]
+  (assert server-name "Can't build server url without a server name")
   (str (name (or scheme :https)) "://" server-name
        (if server-port (str ":" server-port))))
