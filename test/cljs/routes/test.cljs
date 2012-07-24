06d4adb6bed877cdcf07846bd72543639909edcb
Roman Scherer <roman.scherer@nugg.ad>
2012-07-24 14:27:38 +0200

    Fixed clojurescript tests.

--- test/cljs/routes/test.cljs
+++ test/cljs/routes/test.cljs
@@ -1,12 +1,10 @@
 (ns routes.test
   (:require [routes.test.core :as core]
             [routes.test.helper :as helper]
-            [routes.test.repl :as repl]
             [routes.test.server :as server]))
 
 (defn ^:export run []
   (core/test)
   (helper/test)
-  (repl/test)
   (server/test)
   "All tests passed.")
--- test/cljs/routes/test/repl.cljs
+++
@@ -1,8 +0,0 @@
-(ns routes.test.repl
-  (:require [routes.repl :refer [connect]]))
-
-(defn test-connect []
-  (connect))
-
-(defn test []
-  (test-connect))
