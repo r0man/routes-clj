0548f1482f25192e3f566a1c8d4b18a6f84ca7bb
roman <roman@bombaclaat.lan>
2012-07-27 19:23:49 +0200

    Use v8 instead of d8.

--- test-cljs.sh
+++ test-cljs.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-echo "routes.test.run()" | d8 --shell target/routes-test.js
+echo "routes.test.run()" | v8 --shell target/routes-test.js
+echo 'routes.test.run()' | v8 --shell target/routes-test.js
