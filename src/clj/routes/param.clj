d14eb3964d16d6b17c2f198c0df4493dcf7665ba
Roman Scherer <roman.scherer@burningswell.com>
2012-08-05 15:36:13 +0200

    Added routes.param ns.

---
+++ src/clj/routes/param.clj
@@ -0,0 +1,48 @@
+(ns routes.param
+  (:refer-clojure :exclude [format])
+  (:require;*CLJSBUILD-REMOVE*;-macros
+   [routes.core :refer [defparam]])
+  (:require [clojure.string :refer [blank? lower-case]]
+            [inflections.core :refer [parameterize]]
+            [inflections.number :refer [parse-float parse-integer parse-location]]))
+
+(defprotocol IParameter
+  (-format [param obj] "Format `obj` as a route `param`.")
+  (-parse [param s] "Parse `s` into a route `param`."))
+
+(defrecord Parameter [name doc format-fn parse-fn]
+  IParameter
+  (-format [param obj]
+    (if-not (nil? obj) (format-fn obj)))
+  (-parse [param s]
+    (if-not (nil? s) (parse-fn s))))
+
+(defn parse-iso-3166-1-alpha-2 [s]
+  (lower-case (apply str (take 2 (str s)))))
+
+(defn parse-iso-3166-1-alpha-3 [s]
+  (lower-case (apply str (take 3 (str s)))))
+
+(defparam iso-3166-1-alpha-2
+  "A two-letter country code defined in the ISO 3166-1 standard."
+  parse-iso-3166-1-alpha-2
+  parse-iso-3166-1-alpha-2)
+
+(defparam iso-3166-1-alpha-3
+  "A three-letter country code defined in the ISO 3166-1 standard."
+  parse-iso-3166-1-alpha-3
+  parse-iso-3166-1-alpha-3)
+
+(defparam location
+  "A geographical location."
+  #(str (:latitude %1) "," (:longitude %1))
+  parse-location)
+
+(defparam integer
+  "An integer."
+  str
+  parse-integer)
+
+(defparam string
+  "A string."
+  parameterize)
