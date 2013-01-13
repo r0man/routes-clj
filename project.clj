83a769eb8eb9f47ca5c2a168831fa97687b2582f
Roman Scherer <roman.scherer@burningswell.com>
2013-01-13 12:01:23 +0100

    Updated dependencies.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.0.0"
-  :dependencies [[inflections "0.7.4-SNAPSHOT"]
+  :dependencies [[inflections "0.7.5-SNAPSHOT"]
                  [org.clojure/clojure "1.4.0"]]
-  :plugins [[lein-cljsbuild "0.2.9"]]
+  :plugins [[lein-cljsbuild "0.2.10"]]
   :hooks [leiningen.cljsbuild]
   :cljsbuild {:builds [{:compiler {:output-to "target/routes-test.js"
                                    :optimizations :advanced
