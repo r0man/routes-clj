43b0ec9382db288b61634ea042ebdf301da19051
Roman Scherer <roman@burningswell.com>
2014-11-17 22:42:53 +0100

    Version 0.0.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject routes-clj "0.0.4-SNAPSHOT"
+(defproject routes-clj "0.0.4"
   :description "A Clojure & ClojureScript routing library."
   :url "http://github.com/r0man/routes-clj"
   :author "r0man"
