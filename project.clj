4952d47fadd4dd0d4d816aa2a3ffca97b6192bf6
Roman Scherer <roman@burningswell.com>
2015-02-07 14:26:12 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[noencore "0.1.18"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2411" :scope "provided"]]
+                 [org.clojure/clojurescript "0.0-2760" :scope "provided"]]
   :aliases {"ci" ["do" ["cleantest"] ["lint"]]
             "cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]
             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]
@@ -35,11 +35,11 @@
                               "phantom" ["phantomjs" :runner "target/testable.js"]}}
   :deploy-repositories [["releases" :clojars]]
   :prep-tasks [["cljx" "once"]]
-  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.5"]
-                             [com.cemerick/clojurescript.test "0.3.1"]
-                             [jonase/eastwood "0.2.0"]
+  :profiles {:dev {:plugins [[com.cemerick/austin "0.1.7-SNAPSHOT"]
+                             [com.cemerick/clojurescript.test "0.3.3"]
+                             [com.keminglabs/cljx "0.5.0"]
+                             [jonase/eastwood "0.2.1"]
                              [lein-cljsbuild "1.0.3"]
-                             [lein-difftest "2.0.0"]
-                             [org.clojars.cemerick/cljx "0.5.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+                             [lein-difftest "2.0.0"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :test-paths ["target/test-classes"]}})
