1deb73cd4936f89750958ae8ed26af24da428314
Roman Scherer <roman@burningswell.com>
2015-02-10 23:28:32 +0100

    Update ClojureScript

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
             :url "https://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[noencore "0.1.18"]
                  [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2760" :scope "provided"]]
+                 [org.clojure/clojurescript "0.0-2816" :scope "provided"]]
   :aliases {"ci" ["do" ["cleantest"] ["lint"]]
             "cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]
             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]
