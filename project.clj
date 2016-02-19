40053bc228b8f6149481ceffb178f305762966b8
Roman Scherer <roman@burningswell.com>
2016-02-19 23:12:24 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,9 +5,9 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "https://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[noencore "0.1.20"]
-                 [org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3196" :scope "provided"]]
+  :dependencies [[noencore "0.2.0"]
+                 [org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]]
   :aliases {"ci" ["do" ["cleantest"] ["lint"]]
             "cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]
             "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]
@@ -35,11 +35,11 @@
                               "phantom" ["phantomjs" :runner "target/testable.js"]}}
   :deploy-repositories [["releases" :clojars]]
   :prep-tasks [["cljx" "once"]]
-  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.0"]]
+  :profiles {:dev {:dependencies [[com.cemerick/piggieback "0.2.1"]]
                    :plugins [[com.cemerick/clojurescript.test "0.3.3"]
                              [com.keminglabs/cljx "0.6.0"]
-                             [jonase/eastwood "0.2.1"]
-                             [lein-cljsbuild "1.0.5"]
+                             [jonase/eastwood "0.2.3"]
+                             [lein-cljsbuild "1.1.2"]
                              [lein-difftest "2.0.0"]]
                    :repl-options {:nrepl-middleware [cljx.repl-middleware/wrap-cljx]}
                    :test-paths ["target/test-classes"]}})
