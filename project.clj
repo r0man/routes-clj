ef7f497f6834772423de0213109f2f033396d5fe
r0man <roman@burningswell.com>
2017-01-05 21:37:31 +0100

    Version 0.1.12

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject routes-clj "0.1.12-SNAPSHOT"
+(defproject routes-clj "0.1.12"
   :description "A Clojure & ClojureScript routing library."
   :url "https://github.com/r0man/routes-clj"
   :license {:name "Eclipse Public License"
